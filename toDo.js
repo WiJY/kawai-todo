4b6772b3ad420d9ca43c8decdc5cdefd4ceb2980
wijy <wijy1024@gmail.com>
2020-05-04 17:14:56 +0900

    UI Part 2

---
+++ toDo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from "react-native";
+
+export default class Todo extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello, I am Todo</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
